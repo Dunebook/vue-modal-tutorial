bad7cc5f876abf4f316d8a6ec789b95965ada41c
Junior Gantin <nioperas06@gmail.com>
2019-06-26 18:04:46 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vue Modal Tutorial</h1>
+    <button @click="openModal" v-if="!showModal">Open Modal</button>
+    <app-modal v-if="showModal" :showModal=showModal @clicked="onClickChild">
+      <div slot="header">
+        <h3 class="modal-title">
+          Join Dunebook.com today
+        </h3>
+      </div>
+      <div slot="body">
+        <p>
+          Get the best in Python, Angular, React, Vue, NodeJS & more directly to your inbox.
+        </p>
+      </div>
+    </app-modal>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppModal from './components/AppModal';
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
+    AppModal
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    onClickChild () {
+      this.showModal = false;
+    }
+  },
 }
 </script>
-
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
